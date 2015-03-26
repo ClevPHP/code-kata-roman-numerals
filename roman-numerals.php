76c5da17bb66c4e62c0011dd32b3da18cee22789
Jacob McKinney <jmckin02@gmail.com>
2015-03-25 22:48:23 -0400

    Update roman-numerals.php

--- roman-numerals.php
+++ roman-numerals.php
@@ -7,5 +7,28 @@
  * @return integer
  */
 function roman_numerals( $numerals ) {
-
+	return $numbers[$numerals];
 }
+
+$numbers = array(
+	'I' 	=> 1;
+	'i' 	=> 1;
+	'II'	=> 2;
+	'ii'	=> 2;
+	'III' 	=> 3;
+	'iii'	=> 3;
+	'IV'	=> 4;
+	'iv'	=> 4;
+	'V'		=> 5;
+	'v'		=> 5;
+	'VI'	=> 6;
+	'vi'	=> 6;
+	'VII'	=> 7;
+	'vii'	=> 7;
+	'VIII'	=> 8;
+	'viii'	=> 8;
+	'IX'	=> 9;
+	'ix'	=> 9;
+	'X'		=> 10;
+	'x'		=> 10;
+	)
