6aa53e3fe4953fb43d85755f8cc17c2e8c939e43
Jacob McKinney <jmckin02@gmail.com>
2015-03-26 19:02:32 -0400

    Update roman-numerals.php

--- roman-numerals.php
+++ roman-numerals.php
@@ -14,12 +14,12 @@ function roman_numerals( $numerals ) {
 		'II'	=> 2;
 		'III' 	=> 3;
 		'IV'	=> 4;
-		'V'		=> 5;
+		'V'	=> 5;
 		'VI'	=> 6;
 		'VII'	=> 7;
 		'VIII'	=> 8;
 		'IX'	=> 9;
-		'X'		=> 10;
+		'X'	=> 10;
 		'XI'	=> 11;
 		'XII'	=> 12;
 		'XIII'	=> 13;
@@ -32,21 +32,21 @@ function roman_numerals( $numerals ) {
 		'XX'	=> 20;
 		'XXX'	=> 30;
 		'XL'	=> 40;
-		'L'		=> 50;
+		'L'	=> 50;
 		'LX'	=> 60;
 		'LXX'	=> 70;
 		'LXXX'	=> 80;
 		'XC'	=> 90;
-		'C'		=> 100;
+		'C'	=> 100;
 		'CC'	=> 200;
 		'CCC' 	=> 300;
 		'CD'	=> 400;
-		'D'		=> 500;
+		'D'	=> 500;
 		'DC'	=> 600;
 		'DCC'	=> 700;
 		'DCCC'	=> 800;
 		'CM'	=> 900;
-		'M'		=> 1000;
+		'M'	=> 1000;
 		)
 
 	strtoupper( $numerals );
