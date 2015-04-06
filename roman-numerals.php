482228502ca3390e67494518ce2a25e12fe09122
Jacob McKinney <jmckin02@gmail.com>
2015-04-05 23:44:32 -0400

    Update roman-numerals.php

--- roman-numerals.php
+++ roman-numerals.php
@@ -7,52 +7,24 @@
  * @return integer
  */
 
+// khall987's Answer
+
 function roman_numerals( $numerals ) {
 
-	$numbers = array(
-		'I' 	=> 1;
-		'II'	=> 2;
-		'III' 	=> 3;
-		'IV'	=> 4;
-		'V'	=> 5;
-		'VI'	=> 6;
-		'VII'	=> 7;
-		'VIII'	=> 8;
-		'IX'	=> 9;
-		'X'	=> 10;
-		'XI'	=> 11;
-		'XII'	=> 12;
-		'XIII'	=> 13;
-		'XIV'	=> 14;
-		'XV'	=> 15;
-		'XVI'	=> 16;
-		'XVII'	=> 17;
-		'XVIII'	=> 18;
-		'XIX'	=> 19;
-		'XX'	=> 20;
-		'XXX'	=> 30;
-		'XL'	=> 40;
-		'L'	=> 50;
-		'LX'	=> 60;
-		'LXX'	=> 70;
-		'LXXX'	=> 80;
-		'XC'	=> 90;
-		'C'	=> 100;
-		'CC'	=> 200;
-		'CCC' 	=> 300;
-		'CD'	=> 400;
-		'D'	=> 500;
-		'DC'	=> 600;
-		'DCC'	=> 700;
-		'DCCC'	=> 800;
-		'CM'	=> 900;
-		'M'	=> 1000;
-		)
+	$numerals = strtolower( $numerals );
+	$arabic_Numbers = str_split( $numerals );
+	var_dump( $arabic_Numbers );
+	foreach($arabic_Numbers as $numerals) {
+		if($numerals == 'i'){
+			$numerals = 1;
+		} elseif($numerals == 'v') {
+			$numerals = 5;
+		} elseif ($numerals == 'x') {
+			$numerals = 10;
+		}
+	}
+	return($numerals);
 
-	strtoupper( $numerals );
-
-	return $numbers[ $numerals ];
-	
 }
 
 
