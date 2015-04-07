d95efcd5243287fd5a1608ce7f8866872f449025
Jacob McKinney <jmckin02@gmail.com>
2015-04-06 22:06:28 -0400

    Finally completed the excercise

--- roman-numerals.php
+++ roman-numerals.php
@@ -7,24 +7,66 @@
  * @return integer
  */
 
-// klhall987's Answer
 
 function roman_numerals( $numerals ) {
 
 	$numerals = strtolower( $numerals );
-	$arabic_Numbers = str_split( $numerals );
-	var_dump( $arabic_Numbers );
-	foreach($arabic_Numbers as $numerals) {
-		if($numerals == 'i'){
-			$numerals = 1;
-		} elseif($numerals == 'v') {
-			$numerals = 5;
-		} elseif ($numerals == 'x') {
-			$numerals = 10;
+
+	$numbers = str_split( $numerals );
+
+	foreach( $numbers as $key => $roman ) {
+
+		if( $roman == 'i' ) {
+
+			$numbers[ $key ] = 1;
+
+		} elseif( $roman == 'v' ) {
+
+			$numbers[ $key ] = 5;
+
+		} elseif( $roman == 'x' ) {
+
+			$numbers[ $key ] = 10;
+
 		}
 	}
-	return($numerals);
+
+	if ( count( $numbers ) == 1 ) {
+
+		$result = $numbers[ 0 ];
+
+		return $result;
+
+	} elseif ( count( $numbers ) == 2 AND ( ( $numbers[ 0 ] > $numbers[ 1 ] ) || ( $numbers[ 0 ] == $numbers[ 1 ] ) ) ) {
+
+		$result = $numbers[ 0 ] + $numbers[ 1 ];
+
+		return $result;
+
+	} elseif ( count( $numbers ) == 2 AND ( $numbers[ 0 ] < $numbers[ 1 ] ) ) {
+
+		$result = $numbers[ 1 ] - $numbers[ 0 ];
+
+		return $result;
+
+	} elseif ( count( $numbers ) == 3 AND ( $numbers[ 1 ] < $numbers[ 2 ] ) ) {
+
+		$result = $numbers[ 2 ] - $numbers[ 1 ];
+
+		$result = $result + $numbers[ 0 ];
+
+		return $result;
+	} elseif ( count( $numbers ) == 3 ) {
+
+		$result = $numbers[ 0 ] + $numbers[ 1 ] + $numbers[ 2 ];
+
+		return $result;
+	
+	} elseif ( count( $numbers ) == 4 ) {
+
+		$result = $numbers[ 0 ] + $numbers[ 1 ] + $numbers[ 2 ] + $numbers[ 3 ];
+
+		return $result;
+	}
 
 }
-
-
