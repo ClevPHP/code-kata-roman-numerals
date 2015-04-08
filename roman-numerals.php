690b20942ff342c1979a7f12e83e7625229184ae
Jacob McKinney <jmckin02@gmail.com>
2015-04-08 15:09:23 -0400

    Update roman-numerals.php
    Added functionality to be able to calculate all possible Roman Numeral combinations...finally.

--- roman-numerals.php
+++ roman-numerals.php
@@ -7,66 +7,130 @@
  * @return integer
  */
 
-
 function roman_numerals( $numerals ) {
-
 	$numerals = strtolower( $numerals );
-
 	$numbers = str_split( $numerals );
 
 	foreach( $numbers as $key => $roman ) {
-
 		if( $roman == 'i' ) {
-
 			$numbers[ $key ] = 1;
-
 		} elseif( $roman == 'v' ) {
-
 			$numbers[ $key ] = 5;
-
 		} elseif( $roman == 'x' ) {
-
 			$numbers[ $key ] = 10;
-
+		} elseif( $roman == 'l' ) {
+			$numbers[ $key ] = 50;
+		} elseif( $roman == 'c' ) {
+			$numbers[ $key ] = 100;
+		} elseif( $roman == 'd' ) {
+			$numbers[ $key ] = 500;
+		} elseif( $roman == 'm' ) {
+			$numbers[ $key ] = 1000;
 		}
 	}
 
-	if ( count( $numbers ) == 1 ) {
+	// There are ones but no tens or hundreds
+	if( in_array( 1, $numbers ) == TRUE && in_array( 10, $numbers ) == FALSE && in_array( 100, $numbers ) == FALSE  ) {
+		$onekey = array_search( 1, $numbers );
 
-		$result = $numbers[ 0 ];
+		if( $numbers[ $onekey ] < $numbers[ $onekey + 1 ] ) {
+			$numbers[ $onekey ] = -1;
+			$results = array_sum( $numbers );
+			return $results;
+		} else {
+			$results = array_sum( $numbers );
+			return $results;
+		}
+	// There are tens but no ones or hundreds
+	} elseif ( in_array( 1, $numbers ) == FALSE && in_array( 10, $numbers ) == TRUE && in_array( 100, $numbers ) == FALSE ) {
+		$tenkey = array_search( 10, $numbers );
 
-		return $result;
+		if( $numbers[ $tenkey ] < $numbers[ $tenkey + 1 ] ) {
+			$numbers[ $tenkey ] = -10;
+			$results = array_sum( $numbers );
+			return $results;
+		} else {
+			$results = array_sum( $numbers );
+			return $results;
+		}
+	// There are hundreds but no tens or ones
+	} elseif ( in_array( 1, $numbers ) == FALSE && in_array( 10, $numbers ) == FALSE && in_array( 100, $numbers ) == TRUE ) {
+		$hundredkey = array_search( 100, $numbers );
 
-	} elseif ( count( $numbers ) == 2 AND ( ( $numbers[ 0 ] > $numbers[ 1 ] ) || ( $numbers[ 0 ] == $numbers[ 1 ] ) ) ) {
+		if( $numbers[ $hundredkey ] < $numbers[ $hundredkey + 1 ] ) {
+			$numbers[ $hundredkey ] = -100;
+			$results = array_sum( $numbers );
+			return $results;
+		} else {
+			$results = array_sum( $numbers );
+			return $results;
+		}
+	// There are ones and tens but no hundreds
+	} elseif ( in_array( 1, $numbers ) == TRUE && in_array( 10, $numbers ) == TRUE && in_array( 100, $numbers ) == FALSE  ) {
+		$onekey = array_search( 1, $numbers );
+		$tenkey = array_search( 10, $numbers );
 
-		$result = $numbers[ 0 ] + $numbers[ 1 ];
+		if( $numbers[ $onekey ] < $numbers[ $onekey + 1 ] ) {
+			$numbers[ $onekey ] = -1;
+		}
 
-		return $result;
+		if( $numbers[ $tenkey ] < $numbers[ $tenkey + 1 ] ) {
+			$numbers[ $tenkey ] = -10;
+		}
 
-	} elseif ( count( $numbers ) == 2 AND ( $numbers[ 0 ] < $numbers[ 1 ] ) ) {
+		$results = array_sum( $numbers );
+		return $results;
+	// There are tens and hundreds but no ones
+	} elseif ( in_array( 1, $numbers ) == FALSE && in_array( 10, $numbers ) == TRUE && in_array( 100, $numbers ) == TRUE ) {
+		$tenkey = array_search( 10, $numbers );
+		$hundredkey = array_search( 100, $numbers );
 
-		$result = $numbers[ 1 ] - $numbers[ 0 ];
+		if( $numbers[ $tenkey ] < $numbers[ $tenkey + 1 ] ) {
+			$numbers[ $tenkey ] = -10;
+		}
 
-		return $result;
+		if( $numbers[ $hundredkey ] < $numbers[ $hundredkey + 1 ] ) {
+			$numbers[ $hundredkey ] = -100;
+		}
 
-	} elseif ( count( $numbers ) == 3 AND ( $numbers[ 1 ] < $numbers[ 2 ] ) ) {
+		$results = array_sum( $numbers );
+		return $results;
+	// There are ones and hundreds but no tens
+	} elseif ( in_array( 1, $numbers ) == TRUE && in_array( 10, $numbers ) == FALSE && in_array( 100, $numbers ) == TRUE ) {
+		$onekey = array_search( 1, $numbers );
+		$hundredkey = array_search( 100, $numbers );
 
-		$result = $numbers[ 2 ] - $numbers[ 1 ];
+		if( $numbers[ $onekey ] < $numbers[ $onekey + 1 ] ) {
+			$numbers[ $onekey ] = -1;
+		}
 
-		$result = $result + $numbers[ 0 ];
+		if( $numbers[ $hundredkey ] < $numbers[ $hundredkey + 1 ] ) {
+			$numbers[ $hundredkey ] = -100;
+		}
 
-		return $result;
-	} elseif ( count( $numbers ) == 3 ) {
+		$results = array_sum( $numbers );
+		return $results;
+	} elseif ( in_array( 1, $numbers ) == TRUE && in_array( 10, $numbers ) == TRUE && in_array( 100, $numbers ) == TRUE ) {
+		$onekey = array_search( 1, $numbers );
+		$tenkey = array_search( 10, $numbers );
+		$hundredkey = array_search( 100, $numbers );
 
-		$result = $numbers[ 0 ] + $numbers[ 1 ] + $numbers[ 2 ];
+		if( $numbers[ $onekey ] < $numbers[ $onekey + 1 ] ) {
+			$numbers[ $onekey ] = -1;
+		}
 
-		return $result;
-	
-	} elseif ( count( $numbers ) == 4 ) {
+		if( $numbers[ $tenkey ] < $numbers[ $tenkey + 1 ] ) {
+			$numbers[ $tenkey ] = -10;
+		}
 
-		$result = $numbers[ 0 ] + $numbers[ 1 ] + $numbers[ 2 ] + $numbers[ 3 ];
+		if( $numbers[ $hundredkey ] < $numbers[ $hundredkey + 1 ] ) {
+			$numbers[ $hundredkey ] = -100;
+		}
 
+		$results = array_sum( $numbers );
+		return $results;
+	} else {
+		$result = array_sum( $numbers );
 		return $result;
 	}
-
-}
+} // ends roman numerals function
