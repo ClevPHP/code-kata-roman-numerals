b41ce11ffc2d20e267b1e56c79eb0b4c39610a92
Jacob McKinney <jmckin02@gmail.com>
2015-04-08 15:10:53 -0400

    Update index.php
    Added extra tests to ensure success of roman_numerals() function. (including largest possible number that can be represented by roman numerals)

--- index.php
+++ index.php
@@ -18,9 +18,23 @@ $numerals = array(
 	'xii'  => 12,
 	'XIII' => 13,
 	'XIV'  => 14,
-	'XV'   => 15
+	'XV'   => 15,
+	'XL'   => 40,
+	'L'    => 50,
+	'XC'   => 90,
+	'xcix' => 99,
+	'C'    => 100,
+	'CD'   => 400,
+	'd'    => 500,
+	'M'    => 1000,
+	'MCDXLIV' => 1444,
+	'mmmccxxiii' => 3223,
+	'MMMCMXCIX' => 3999,
 );
+
 
 foreach( $numerals as $test => $answer ) {
 	echo $test . ' is ' . roman_numerals( $test ) . ". " . check('roman_numerals', $test, $answer) . "\n";
 }
+
+
